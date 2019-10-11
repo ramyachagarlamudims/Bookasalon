f58c3577f14a37bde8422b4054ca458bfcfb95a3
chagarla.ram <chagarla.ram@gmail.com>
2019-10-11 15:25:59 +0530

    intial build

--- src/test/java/com/bookasalon/org/ApplicationTests.java
+++ src/test/java/com/bookasalon/org/ApplicationTests.java
@@ -1,12 +1,18 @@
 package com.bookasalon.org;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ApplicationTests {
 
-
+@Test
 	public void contextLoads() {
+		assertEquals(true,true);
 	}
+
+
 
 }
